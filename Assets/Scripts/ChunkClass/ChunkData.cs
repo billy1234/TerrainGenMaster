558255fa56f12af59ce7fe29218f88678d754d8e
Billy Mihalarias <bmihalarias@gmail.com>
2015-10-21 16:19:32 +1100

    added a basic ui so users may create their own passes.

--- Assets/Scripts/ChunkClass/ChunkData.cs
+++ Assets/Scripts/ChunkClass/ChunkData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 public enum MAPDATACHANNEL
 {
 	R,G,B,A
---
+++ Assets/Scripts/ChunkClass/myTerrain.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public struct fillType
+{
+	public MAPDATACHANNEL channel;
+	public float zoomLevel;
+};
+
+[System.Serializable]
+public struct terrainPasses
+{
+	public fillAllrandom fillAllrandomSeed;
+	public fillType[] randomPerlinFill;
+	public clampPass[] clampPasses;
+}
+[System.Serializable]
+public struct fillAllrandom
+{
+	public bool run;
+	public float zoomLevel;
+}
+
+
+
+
+[RequireComponent(typeof(Renderer))]
+public class myTerrain : MonoBehaviour
+{
+	protected ChunkData terrainMaps;
+	public terrainPasses passes;
+	public int size = 100;
+	void Start()
+	{
+		terrainMaps = new ChunkData(size);
+
+		if(passes.fillAllrandomSeed.run)
+		{
+			terrainMaps.fillAllWithRandomPerlin(passes.fillAllrandomSeed.zoomLevel);
+		}
+
+		foreach(clampPass p in passes.clampPasses)
+		{
+			terrainMaps.clampChannelValues(p);
+		}
+		foreach(fillType f in passes.randomPerlinFill)
+		{
+			terrainMaps.fillWithPerlin(f.zoomLevel,f.channel);
+		}
+		gameObject.GetComponent<Renderer>().material.mainTexture = terrainMaps.mapCells;
+	}
+
+}
