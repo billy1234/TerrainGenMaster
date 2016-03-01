fa83409f404291f85f79fabebece9eb03bf0cec4
Billy Mihalarias <bmihalarias@gmail.com>
2016-03-01 22:47:23 +1100

    implemented a more simple 3d cellular automata class, witch yielded some interesting results.

--- Assets/Scripts/CellularAutomata/CATest.cs
+++ Assets/Scripts/CellularAutomata/CATest.cs
@@ -4,25 +4,66 @@ using CellularAutomataLibary;
 public class CATest : MonoBehaviour 
 {
 	public int sizeX =5,sizeY =5,sizeZ=5;
+	GameObject[,,] displayCubes;
+	public GameObject cell;
+	ThreeDimentionalCA ca;
+	public int initalPoints = 30;
 
 	void Start () 
 	{
-		ThreeDimentionalCA ca = new ThreeDimentionalCA(new int[3]{sizeX,sizeY,sizeZ});
+		ca = new ThreeDimentionalCA(sizeX,sizeY,sizeZ);
+		displayCubes = new GameObject[sizeX,sizeY,sizeZ];
+		initDraw();
+		ca.addRandomPoints(initalPoints);
+
+		draw();
+		StartCoroutine(runCa());
+
+	}
+
+	IEnumerator runCa()
+	{
+		yield return new WaitForSeconds(1f);
+		while(true)
+		{
+			stepGeneration();
+			yield return new WaitForSeconds(1f);
+		}
+	}
+
+	void stepGeneration()
+	{
+		ca.runGeneration();
+		draw();
+
+	}
+
+	void draw()
+	{
 		for(int x =0; x <sizeX; x++)
 		{
 			for(int y =0; y <sizeY; y++)
 			{
 				for(int z =0; z <sizeZ; z++)
 				{
-					print(" "+x+" "+y+" "+z);
+					displayCubes[x,y,z].SetActive(ca.cells[x,y,z]);
 				}
 			}
 		}
-
+	}
+	void initDraw()
+	{
+		for(int x =0; x <sizeX; x++)
+		{
+			for(int y =0; y <sizeY; y++)
+			{
+				for(int z =0; z <sizeZ; z++)
+				{
+					displayCubes [x,y,z] =  (GameObject)Instantiate(cell,new Vector3(x,y,z),Quaternion.identity);
+				}
+			}
+		}
 	}
 	
-
-	void Update () {
 	
-	}
 }
--- Assets/Scripts/CellularAutomata/CellularAutomataLibary.cs
+++ Assets/Scripts/CellularAutomata/CellularAutomataLibary.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Reflection;
 namespace CellularAutomataLibary
 {
+	/*
 	public class CellularAutomataCell
 	{
 		bool state = true;
@@ -66,7 +67,7 @@ namespace CellularAutomataLibary
 
 
 	}
-		public class ThreeDimentionalCA : CellularAutomota
+	public class ThreeDimentionalCA : CellularAutomota
 		{
 			private static readonly int[,] neighbors = new int[8,2]{{-1,-1},{1,1},{-1,1},{1,-1},{0,1},{0,-1},{-1,0},{1,0}};
 			private readonly int heightIndex =0;
@@ -88,6 +89,90 @@ namespace CellularAutomataLibary
 				return cells[indexes[0] + (dimentionDepths[widthIndex] -1) * (indexes[1] + (dimentionDepths[depthIndex]-1) * indexes[2])];
 			}
 		}
+		*/
 
-	
+	public class ThreeDimentionalCA
+	{
+		public bool [,,] cells;
+		readonly int width,length,depth;
+		public ThreeDimentionalCA(int width,int length,int depth)
+		{
+			this.width = width;
+			this.length = length;
+			this.depth = depth;
+			cells = new bool[width,length,depth];
+		}
+		public void addRandomPoints(int points)//initalizer fucntion
+		{
+			for(int i=0; i < points; i++)
+			{
+				cells[Random.Range(0,width),Random.Range(0,length),Random.Range(0,depth)] = true;
+			}
+		}
+		public void runGeneration()
+		{
+			for(int x=0; x < width; x++)
+			{
+				for(int y=0; y < length; y++)
+				{
+					for(int z=0; z < width; z++)
+					{
+						int neighbors = getNeighbors(x,y,z);
+						if(cells[x,y,z] == true)//live cell rules
+						{
+
+							if(neighbors < 2)//Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+							{
+								cells[x,y,z] = false;
+							}
+							else if(neighbors > 3)//Any live cell with more than three live neighbours dies, as if by over-population.
+							{
+								cells[x,y,z] = false;
+							}
+							//Any live cell with two or three live neighbours lives on to the next generation.
+
+						}
+						else//dead cells
+						{
+							//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+							if(neighbors ==3)
+							{
+								cells[x,y,z] = true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public int getNeighbors(int positionX, int positionY, int positionZ)
+		{
+			int cellCount =0;
+			int arrayX,arrayY,arrayZ;
+			for(int x=-1; x < 2; x++)
+			{
+				for(int y=-1; y < 2; y++)
+				{
+					for(int z=-1; z < 2; z++)
+					{
+						if(x != 0 || y != 0 || z != 0)
+						{
+							arrayX = x+ positionX;
+							arrayY = y+ positionY;
+							arrayZ = z+ positionZ;
+
+							if(arrayX > 0 && arrayY > 0 && arrayZ > 0 && arrayX < width && arrayY < length&&arrayZ < depth)
+							{
+								if(cells[arrayX,arrayY,arrayZ] == true)
+								{
+									cellCount++;
+								}
+							}
+						}
+					}
+				}
+			}
+			return cellCount;
+		}
+	}
 }
