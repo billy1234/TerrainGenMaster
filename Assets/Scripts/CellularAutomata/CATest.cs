f374b4788ef284389603df347dda428cec20e288
Billy Mihalarias <bmihalarias@gmail.com>
2016-03-01 16:20:44 +1100

    began writing a cellular automata class that will support all dimensions, having issues instantiating ambiguous typed cells.

---
+++ Assets/Scripts/CellularAutomata/CATest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using CellularAutomataLibary;
+public class CATest : MonoBehaviour 
+{
+	public int sizeX =5,sizeY =5,sizeZ=5;
+
+	void Start () 
+	{
+		ThreeDimentionalCA ca = new ThreeDimentionalCA(new int[3]{sizeX,sizeY,sizeZ});
+		for(int x =0; x <sizeX; x++)
+		{
+			for(int y =0; y <sizeY; y++)
+			{
+				for(int z =0; z <sizeZ; z++)
+				{
+					print(" "+x+" "+y+" "+z);
+				}
+			}
+		}
+
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CellularAutomata/CellularAutomataLibary.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+namespace CellularAutomataLibary
+{
+	public class CellularAutomataCell
+	{
+		bool state = true;
+
+
+	}
+	public class CellularAutomataCellSimple : CellularAutomataCell
+	{
+		public bool state;
+	}
+
+	public abstract class CellularAutomota
+	{
+		public readonly int dimentions;
+		public readonly int[] dimentionDepths;
+		private System.Type cellType;
+		public CellularAutomataCell[] cells;
+		public CellularAutomota(int[] dimentionDepths,int dimentions,System.Type cellType)
+		{
+			this.dimentionDepths = dimentionDepths;
+			this.dimentions = dimentions;
+			this.cellType = cellType;
+			#region initalize cell heap
+			int cellCount =0;
+			for(int i=0; i< dimentionDepths.Length;i++)
+			{
+				if(i ==0)
+				{
+					cellCount = dimentionDepths[i];
+				}
+				else
+				{
+					cellCount *= dimentionDepths[i];
+					//
+				}
+			}
+			this.cells =null; //cellType[cellCount];(need to use celltype to insanciate the cells)
+
+
+			for(int i=0; i< cells.Length;i++)
+			{
+				//cells[i] = new CellularAutomataCell();
+			}
+			#endregion
+		}
+		protected abstract CellularAutomataCell getCellFromChildClass(params int[] indexes);//the ca must return a cell given indexes
+
+		public CellularAutomataCell getCell(params int[] indexes)
+		{
+			if(indexes.Length != dimentions)
+			{
+				Debug.LogError("Error," +this+" Was passed "+indexes+" But has "+dimentions+ " dimentions");
+			}
+			else
+			{
+				return getCellFromChildClass(indexes);
+			}
+			return null;
+
+		}
+
+
+	}
+		public class ThreeDimentionalCA : CellularAutomota
+		{
+			private static readonly int[,] neighbors = new int[8,2]{{-1,-1},{1,1},{-1,1},{1,-1},{0,1},{0,-1},{-1,0},{1,0}};
+			private readonly int heightIndex =0;
+			private readonly int widthIndex =1;
+			private readonly int depthIndex =2;
+
+			public ThreeDimentionalCA(int[] dimentionDepths):base(dimentionDepths,3)
+			{
+				if(dimentionDepths.Length != dimentions)
+				{
+					Debug.LogError("Error," +this+" Was passed "+dimentionDepths+" But has "+dimentions+ " dimentions");
+				}
+			}
+
+			protected override CellularAutomataCell getCellFromChildClass(params int[] indexes)
+			{
+				//Debug.Log(dimentionDepths[depthIndex]-1);
+				Debug.Log(indexes[0] + (dimentionDepths[widthIndex] -1) * (indexes[1] + (dimentionDepths[depthIndex]-1) * indexes[2]));
+				return cells[indexes[0] + (dimentionDepths[widthIndex] -1) * (indexes[1] + (dimentionDepths[depthIndex]-1) * indexes[2])];
+			}
+		}
+
+	
+}
